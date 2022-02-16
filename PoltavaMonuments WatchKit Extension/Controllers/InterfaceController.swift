a558f15fcce12711f2feee380c2b9fd3b069f5c4
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-02-16 23:57:00 +0200

    update main screen

--- PoltavaMonuments WatchKit Extension/Controllers/InterfaceController.swift
+++ PoltavaMonuments WatchKit Extension/Controllers/InterfaceController.swift
@@ -17,27 +17,43 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Lifecycle
     
     override func willActivate() {
-        NetworkManager.shared.requestImageUrls() { [weak self] stringUrls in
+        let locationManager = LocationManager()
+        
+        NetworkManager.shared.requestImageUrls(location: locationManager.currentLocation.coordinate) { [weak self] stringUrls in
             guard let stringUrls = stringUrls else { return }
+            
+            self?.setupTableView(with: stringUrls.map { UIImage(named: $0) ?? .init()}, placeholderMode: true)
             
             ImageLoader.shared.loadImages(from: stringUrls) { images in
                 self?.setupTableView(with: images)
             }
         }
     }
-
+    
+    // MARK: - User interactions
+    
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        super.table(table, didSelectRowAt: rowIndex)
+        
+        //detect of tap on row
+    }
+    
 }
 
 // MARK: - Setup
 
 private extension InterfaceController {
     
-    func setupTableView(with images: [UIImage]) {
+    func setupTableView(with images: [UIImage], placeholderMode: Bool = false) {
+        let locationManager = LocationManager()
+        let lat = locationManager.currentLocation.coordinate.latitude
+        let lon = locationManager.currentLocation.coordinate.longitude
         table.setNumberOfRows(images.count, withRowType: "MyRow")
         
         for (index, image) in images.enumerated() {
             if let controller = table.rowController(at: index) as? MainTable {
-                controller.set(image: image)
+                controller.set(image: placeholderMode ? .placeholder : image)
+                controller.set(text: "lat: \(lat) lon: \(lon)")
             }
         }
     }
