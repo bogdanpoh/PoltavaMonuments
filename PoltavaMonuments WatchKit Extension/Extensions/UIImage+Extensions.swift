3a95290c4824eff5b70d265f638735d5e9a2921c
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-02-16 23:55:53 +0200

    added create placeholder image for fast use in code

---
+++ PoltavaMonuments WatchKit Extension/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+Extensions.swift
+//  PoltavaMonuments WatchKit Extension
+//
+//  Created by Bohdan Pokhidnia on 16.02.2022.
+//
+
+import WatchKit
+
+extension UIImage {
+    
+    static var placeholder: UIImage? {
+        return UIImage(named: "placeholder")
+    }
+    
+}
