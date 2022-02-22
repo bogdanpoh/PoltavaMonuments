5335a50e013fe73a2920f1d717475a50ea1469b5
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-02-22 14:42:30 +0200

    added download condition-status of monument and check for request

--- PoltavaMonuments WatchKit Extension/Controllers/InterfaceController.swift
+++ PoltavaMonuments WatchKit Extension/Controllers/InterfaceController.swift
@@ -16,11 +16,19 @@ class InterfaceController: WKInterfaceController {
     
     // MARK: - Lifecycle
     
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        checkCondition()
+    }
+    
     override func willActivate() {
+        super.willActivate()
+        
         guard monuments == nil else { return }
         let locationManager = LocationManager()
         
-        NetworkManager.shared.requestMonuments(location: locationManager.currentLocation.coordinate) { [weak self] responseResult in
+        NetworkManager.shared.requestMonuments(coordinate: locationManager.currentLocation.coordinate) { [weak self] responseResult in
             switch responseResult {
             case .success(let monuments):
                 self?.monuments = monuments
@@ -61,16 +69,43 @@ extension InterfaceController {
 private extension InterfaceController {
     
     func setupTable(_ monuments: [Monument]) {
-        table.setNumberOfRows(monuments.count, withRowType: "MyRow")
+        let filteredMonuments = monuments.filter({ !$0.tags.contains("easter_egg") })
+        table.setNumberOfRows(filteredMonuments.count, withRowType: "MyRow")
         
-        for (index, monument) in monuments.enumerated() {
+        for (index, monument) in filteredMonuments.enumerated() {
             if let controller = table.rowController(at: index) as? MainTable {
                 controller.set(text: monument.name)
                 controller.set(image: .placeholder)
                 
-                ImageLoader.shared.loadImage(url: monument.majorPhotoImageUrl) { image in
-                    controller.set(image: image)
+                if monument.condition.status == .lost {
+                    let badgeTitle = UserDefaults.standard.string(forKey: monument.condition.abbreviation) ?? monument.condition.abbreviation
+                    controller.setBadge(text: badgeTitle)
                 }
+                
+                ImageLoader.shared.loadImage(url: monument.majorPhotoImageUrl) { [weak controller] image in
+                    controller?.set(image: image)
+                }
+            }
+        }
+    }
+    
+}
+
+// MARK: - Check save condition
+
+private extension InterfaceController {
+    
+    func checkCondition() {
+        guard !UserDefaults.standard.bool(forKey: "isCondition") else { return }
+        
+        NetworkManager.shared.requestCondition { result in
+            switch result {
+            case .success(let conditions):
+                UserDefaults.standard.set(true, forKey: "isCondition")
+                conditions.forEach { UserDefaults.standard.set($0.name ?? $0.abbreviation, forKey: $0.abbreviation) }
+                
+            case .failure(let error):
+                print("[dev] error request condition: \(error)")
             }
         }
     }
